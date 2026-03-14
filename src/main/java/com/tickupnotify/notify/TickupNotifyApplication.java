3e611fb1245a0f0e6fc4ecfa3899068f723866d5
Samuel Kébé <samuelkebe09.skb@gmail.com>
2026-03-14 14:06:36 +0100

    Mail Config

---
+++ src/main/java/com/tickupnotify/notify/TickupNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.tickupnotify.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickiupNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickiupNotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tickupnotify/notify/TickiupNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickupnotify.notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickiupNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
